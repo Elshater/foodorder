8ae6db52b876affc8029d9701cecc68a65f0aa3e
elshater <elshater.ehab@gmail.com>
2022-10-30 00:48:02 +0200

    adding meal component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from "./mealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h1>Delicious food</h1>
+
+      <p>choose yor fevorite meal</p>
+      <p>all melas are cooked with high quality</p>
+    </section>
+  );
+};
+export default MealsSummary;
